d29a1d9e8bc99b6860e98e7f18862a8b7debcd0a
Zhu Liang <zl4589130@Gmail.com>
2015-04-10 20:16:22 +0800

    新增切换城市和手动更新天气的功能

--- src/com/coolweather/app/activity/WeatherActivity.java
+++ src/com/coolweather/app/activity/WeatherActivity.java
@@ -6,16 +6,19 @@ import com.coolweather.app.util.HttpUtil;
 import com.coolweather.app.util.Utility;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class WeatherActivity extends Activity {
+public class WeatherActivity extends Activity implements OnClickListener{
 	private LinearLayout weatherInfoLayout;
 	
 	private TextView cityNameText;
@@ -29,6 +32,10 @@ public class WeatherActivity extends Activity {
 	private TextView temp2Text;
 	
 	private TextView currentDateText;
+	
+    private Button switchCity;
+	
+	private Button refreshWeather;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
@@ -50,6 +57,32 @@ public class WeatherActivity extends Activity {
 			queryWeatherCode(countyCode);
 		}else{
 			showWeather();//这种情况是因为一进去程序就是显示天气，所以没有进行城市选择，所以countyCode也是空的
+		}
+		switchCity=(Button)findViewById(R.id.switch_city);
+		refreshWeather=(Button)findViewById(R.id.refresh_weather);
+		switchCity.setOnClickListener(this);
+		refreshWeather.setOnClickListener(this);
+	}
+	
+	@Override
+	public void onClick(View v){
+		switch(v.getId()){
+			case R.id.switch_city:
+				Intent intent=new Intent(this,ChooseAreaActivity.class);
+				intent.putExtra("from_weather_activity", true);
+				startActivity(intent);
+				finish();
+				break;
+			case R.id.refresh_weather:
+				publishText.setText("同步中...");
+				SharedPreferences prefs=PreferenceManager.getDefaultSharedPreferences(this);
+				String weatherCode=prefs.getString("weather_code", "");
+				if(!TextUtils.isEmpty(weatherCode)){
+					queryWeatherInfo(weatherCode);
+				}
+				break;
+			default:
+				break;
 		}
 	}
 	
